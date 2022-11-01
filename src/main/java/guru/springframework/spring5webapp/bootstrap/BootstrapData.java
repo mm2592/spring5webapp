74c2072591008d7c659fc79f7cec8b03dee4c783
Michael Museler <michael.museler@valtech>
2022-11-01 22:27:52 +0100

    add publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
   private final AuthorRepository authorRepository;
   private final BookRepository bookRepository;
+  private final PublisherRepository publisherRepository;
 
-  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
     this.authorRepository = authorRepository;
     this.bookRepository = bookRepository;
+    this.publisherRepository = publisherRepository;
   }
 
   @Override
@@ -37,7 +41,11 @@ public class BootstrapData implements CommandLineRunner {
     authorRepository.save(rod);
     bookRepository.save(noEJB);
 
+    Publisher publisher = new Publisher("Andrew", "Bakerstreet", "New York City", "New York", "898989");
+    publisherRepository.save(publisher);
+
     System.out.println("Started at Bootstrap");
     System.out.println("Number of books: " + bookRepository.count());
+    System.out.println("Number of publisher: " + publisherRepository.count());
   }
 }
